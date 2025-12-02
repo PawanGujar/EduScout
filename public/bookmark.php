d982c4ef24b2cb07a93c2b9854972d5927335b6f
PawanGujar <pawankumar87gujar@gmail.com>
2025-12-02 23:58:24 +0500

    updated bookmark.php

--- public/bookmark.php
+++ public/bookmark.php
@@ -1,7 +1,8 @@
 <?php
 // bookmark.php
 session_start();
-require_once 'config.php';
+require_once __DIR__ . '/../config/config.php';
+header('Content-Type: application/json');
 
 // Only logged-in learners can bookmark
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'learner') {
